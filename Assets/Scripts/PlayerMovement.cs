c268f17912e0cc01378d02e1c94b3b72ba19a198
Brian Moulia <mouliabrian@gmail.com>
2026-09-03 02:59:53 -0300

    gravedad del juegador y movimiento thirdpersonshooter horizontal

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,9 @@ public class PlayerMovement : MonoBehaviour
 
     private CharacterController controller;
     public float movementSpeed = 5.0f;
-
+    public float mouseSensibility = 200.0f;
+    public float gravity = -9.81f;
+    private float velocidadVertical;
 
 
 
@@ -18,11 +20,29 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //gravedad
+        if(controller.isGrounded && velocidadVertical < 0)
+        {
+            velocidadVertical = -2f;
+        }
+        //caida
+        velocidadVertical += gravity * Time.deltaTime;
+
+        //movimiento del mouse
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensibility * Time.deltaTime;
+        transform.Rotate(0, mouseX, 0);
+
+
+        //movimiento del personaje con "WASD"
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-
-        Vector3 direccion = new Vector3(x, 0, y);
-
+        Vector3 direccion = transform.right * x + transform.forward * y;
         controller.Move(direccion * movementSpeed * Time.deltaTime);
+
+        Vector3 movimiento = direccion * movementSpeed;
+        movimiento.y = velocidadVertical;
+
+        controller.Move(movimiento * Time.deltaTime);
     }
 }
